0e4af2fc515918bbd950c82e9fc9ffb0d409391f
Dmitry Sidorenko <rustydragon@gmail.com>
2010-07-26 20:15:08 +0400

    Some examples

---
+++ src/test/java/com/googlecode/commandme/impl/ArgumentsParserTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.commandme.impl;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ushkinaz
+ */
+public class ArgumentsParserTest {
+
+    public ArgumentsParserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of parse method, of class ArgumentsParser.
+     */
+    @Test
+    public void testParse() {
+        fail("The test case is a prototype.");
+    }
+
+}
