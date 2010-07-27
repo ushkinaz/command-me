cf811a493028b7f9a84ad299d17d06184762bc38
Dmitry Sidorenko <rustydragon@gmail.com>
2010-07-27 17:39:41 +0400

    Some stuff

--- src/main/java/com/googlecode/commandme/impl/ArgumentsParser.java
+++ src/main/java/com/googlecode/commandme/impl/ArgumentsParser.java
@@ -3,6 +3,7 @@ package com.googlecode.commandme.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,12 +15,15 @@ class ArgumentsParser {
     @SuppressWarnings({"UnusedDeclaration"})
     private static final Logger LOGGER = LoggerFactory.getLogger(ArgumentsParser.class);
 
+    private String[] arguments;
+
     /**
      * Creates a parser
      *
      * @param arguments arguments to parse
      */
     public ArgumentsParser(String[] arguments) {
+        this.arguments = arguments;
     }
 
     /**
@@ -28,6 +32,12 @@ class ArgumentsParser {
      * @return result of parsing. Never {@code null}
      */
     public List<CliParameter> parse() {
+        List<CliParameter> result = new ArrayList<CliParameter>();
+
+//        CliParameter parameter = new
+        for (String arg: arguments){
+
+        }
         return null;
     }
 }
--- src/test/java/com/googlecode/commandme/impl/ArgumentsParserTest.java
+++ src/test/java/com/googlecode/commandme/impl/ArgumentsParserTest.java
@@ -1,4 +1,20 @@
 /*
+ * Copyright (c) 2010-2010, Dmitry Sidorenko. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -7,13 +23,19 @@ package com.googlecode.commandme.impl;
 
 import org.junit.*;
 
-import static org.junit.Assert.fail;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 /**
  *
  * @author ushkinaz
  */
 public class ArgumentsParserTest {
+    private ArgumentsParser argumentsParser;
+    private static final String[] ARGUMENTS = {"-abc", "--name", "\"John Deere\"", "-r", "1", "commit"};
+
 
     public ArgumentsParserTest() {
     }
@@ -28,18 +50,49 @@ public class ArgumentsParserTest {
 
     @Before
     public void setUp() {
+        argumentsParser = new ArgumentsParser(ARGUMENTS);
     }
 
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of parse method, of class ArgumentsParser.
-     */
     @Test
-    public void testParse() {
-        fail("The test case is a prototype.");
+    public void testParseConcatenated() {
+        argumentsParser = new ArgumentsParser(new String[]{"-abc"});
+        List<CliParameter> parsed = argumentsParser.parse();
+        assertThat(parsed.size(), is(3));
+
+        assertThat(parsed.get(0).getName(), is("a"));
+        assertThat(parsed.get(0).getValue(), nullValue());
+
+        assertThat(parsed.get(1).getName(), is("b"));
+        assertThat(parsed.get(1).getValue(), nullValue());
+
+        assertThat(parsed.get(2).getName(), is("c"));
+        assertThat(parsed.get(2).getValue(), nullValue());
     }
 
+    @Test
+    public void testParseN() {
+        argumentsParser = new ArgumentsParser(new String[]{"-abc"});
+        List<CliParameter> parsed = argumentsParser.parse();
+        assertThat(parsed.size(), is(3));
+
+        assertThat(parsed.get(0).getName(), is("a"));
+        assertThat(parsed.get(0).getValue(), nullValue());
+
+        assertThat(parsed.get(1).getName(), is("b"));
+        assertThat(parsed.get(1).getValue(), nullValue());
+
+        assertThat(parsed.get(2).getName(), is("c"));
+        assertThat(parsed.get(2).getValue(), nullValue());
+    }
+
+    @Test
+    public void testParseNotNull() {
+        argumentsParser = new ArgumentsParser(new String[]{"-abc"});
+        List<CliParameter> parsed = argumentsParser.parse();
+        assertThat(parsed, notNullValue());
+    }
 }
