b8cce182a47ae8391da349b1a8468535c6146aee
Dmitry Sidorenko <rustydragon@gmail.com>
2010-10-01 12:35:25 +0400

    Sva changes

--- src/main/java/com/googlecode/commandme/impl/interrogator/Interrogator.java
+++ src/main/java/com/googlecode/commandme/impl/interrogator/Interrogator.java
@@ -1,26 +1,41 @@
 package com.googlecode.commandme.impl.interrogator;
 
 import com.googlecode.commandme.impl.introspector.ModuleParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Interrogates an instance, injects values of parameters and calls actions
+ * Interrogates an instance, injects values of arguments and calls actions
  *
  * @author Dmitry Sidorenko
  */
 public class Interrogator<T> {
+    @SuppressWarnings({"UnusedDeclaration"})
+    private static final Logger LOGGER = LoggerFactory.getLogger(Interrogator.class);
+
+    private T module;
+    private ModuleParameters moduleParameters;
+    private String[] arguments;
+
     /**
      * A constructor.
      *
      * @param instance         an instance to interrogate
      * @param moduleParameters definitions
-     * @param parameters       actual parameters
+     * @param arguments       actual arguments
      */
-    Interrogator(Object instance, ModuleParameters moduleParameters, String[] parameters) {
+    Interrogator(T instance, ModuleParameters moduleParameters, String[] arguments) {
+        this.module = instance;
+        this.moduleParameters = moduleParameters;
+        this.arguments = arguments;
     }
 
     /**
      * Does actual injecting and calls actions
      */
     public void torture() {
+        for (int i = 0; i < arguments.length; i++){
+            LOGGER.debug(arguments[i]);
+        }
     }
 }
