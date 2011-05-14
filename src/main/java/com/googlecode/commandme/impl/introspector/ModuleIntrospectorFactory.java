c706ff206653a8d3659cf2b086eede619e0681df
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-14 14:40:04 +0400

    Updated copyright

--- src/main/java/com/googlecode/commandme/impl/introspector/ModuleIntrospectorFactory.java
+++ src/main/java/com/googlecode/commandme/impl/introspector/ModuleIntrospectorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2010, Dmitry Sidorenko. All Rights Reserved.
+ * Copyright (c) 2010-2011, Dmitry Sidorenko. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ public class ModuleIntrospectorFactory {
         factory = new ModuleIntrospectorFactory();
     }
 
-    public static <T> ModuleIntrospector createIntrospector(Class clz){
+    public static <T> ModuleIntrospector createIntrospector(Class clz) {
         return factory.create(clz);
     }
 
