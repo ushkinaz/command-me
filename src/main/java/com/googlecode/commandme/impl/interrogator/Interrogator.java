b09ae7461e3539e11ad0ff45fabb9417a9b67ccb
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-14 14:40:04 +0400

    Updated copyright

--- src/main/java/com/googlecode/commandme/impl/interrogator/Interrogator.java
+++ src/main/java/com/googlecode/commandme/impl/interrogator/Interrogator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2011, Dmitry Sidorenko. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.googlecode.commandme.impl.interrogator;
 
 import com.googlecode.commandme.impl.introspector.ModuleParameters;
@@ -22,7 +38,7 @@ public class Interrogator<T> {
      *
      * @param instance         an instance to interrogate
      * @param moduleParameters definitions
-     * @param arguments       actual arguments
+     * @param arguments        actual arguments
      */
     Interrogator(T instance, ModuleParameters moduleParameters, String[] arguments) {
         this.module = instance;
@@ -34,7 +50,7 @@ public class Interrogator<T> {
      * Does actual injecting and calls actions
      */
     public void torture() {
-        for (int i = 0; i < arguments.length; i++){
+        for (int i = 0; i < arguments.length; i++) {
             LOGGER.debug(arguments[i]);
         }
     }
--- src/main/java/com/googlecode/commandme/impl/introspector/ParameterDefinition.java
+++ src/main/java/com/googlecode/commandme/impl/introspector/ParameterDefinition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2010, Dmitry Sidorenko. All Rights Reserved.
+ * Copyright (c) 2010-2011, Dmitry Sidorenko. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ public class ParameterDefinition {
     }
 
     public void setShortName(String shortName) {
-        if (this.shortName!= null) {
+        if (this.shortName != null) {
             throw new IllegalStateException("ShortName can't be set twice");
         }
         this.shortName = shortName;
