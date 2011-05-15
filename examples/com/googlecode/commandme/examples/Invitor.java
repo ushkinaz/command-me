b72f4136b8776ce45d8b52b54f2277cb3177ef48
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-15 13:08:48 +0400

    Moved sample
    --HG--
    rename : src/test/java/com/googlecode/commandme/samples/Invitor.java => examples/com/googlecode/commandme/examples/Invitor.java

---
+++ examples/com/googlecode/commandme/examples/Invitor.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2010-2011, Dmitry Sidorenko. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.googlecode.commandme.examples;
+
+import com.googlecode.commandme.annotations.Action;
+import com.googlecode.commandme.annotations.Parameter;
+
+/**
+ * @author ushkinaz
+ */
+public class Invitor {
+
+    @Parameter
+    public void setName(String name) {
+
+    }
+
+    @Parameter
+    public void setMoney(int amount) {
+
+    }
+
+    @Action
+    public void invite() {
+
+    }
+}
