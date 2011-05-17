54cab69a7046d7cdf8b8e817f510c42466824d36
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-17 18:03:30 +0400

    [starts #13446725] Initial story test
    --HG--
    rename : examples/com/googlecode/commandme/examples/HelloWorld.java => src/test/java/com/googlecode/commandme/story/Story_13446725.java

---
+++ src/test/java/com/googlecode/commandme/story/Story_13446725.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2010-2010, Dmitry Sidorenko. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.commandme.story;
+
+import com.googlecode.commandme.CLIParser;
+import com.googlecode.commandme.CommandLine;
+import com.googlecode.commandme.annotations.Action;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Execute a method marked as @Action
+ * <p/>
+ * <a href="https://www.pivotaltracker.com/story/show/13446725">story</a>
+ *
+ * @author Dmitry Sidorenko
+ */
+public class Story_13446725 {
+
+    private boolean called = false;
+
+    @Action
+    public void greet() {
+        System.out.println("Hello");
+        called = true;
+    }
+
+    @Test
+    public void testStory() throws Exception {
+        CommandLine<Story_13446725> module = CLIParser.createModule(Story_13446725.class);
+        module.execute(new String[]{"greet"});
+
+        assertThat("Method was not called", called, is(true));
+    }
+
+    @Test
+    public void testNegativeStory() throws Exception {
+        CommandLine<Story_13446725> module = CLIParser.createModule(Story_13446725.class);
+        module.execute(new String[]{});
+
+        assertThat("Method was called", called, is(false));
+    }
+}
