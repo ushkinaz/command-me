4ceb75ad5ea742519b994beab6f541bafe7c6af3
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-18 21:08:56 +0400

    [continues #13446727]  Initial work

--- src/test/java/com/googlecode/commandme/impl/interrogator/InterrogatorTest.java
+++ src/test/java/com/googlecode/commandme/impl/interrogator/InterrogatorTest.java
@@ -1,9 +1,10 @@
 package com.googlecode.commandme.impl.interrogator;
 
+import com.googlecode.commandme.annotations.Parameter;
 import com.googlecode.commandme.impl.introspector.ModuleActions;
 import com.googlecode.commandme.impl.introspector.ModuleIntrospector;
 import com.googlecode.commandme.impl.introspector.ModuleParameters;
-import junit.framework.Assert;
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,10 +13,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class InterrogatorTest {
-    private Interrogator<InterrogatorTest>       interrogator;
+    private static final String JOHN_FROSTER = "John Froster";
     private ModuleIntrospector<InterrogatorTest> moduleIntrospector;
     private ModuleActions                        moduleActions;
     private ModuleParameters                     moduleParameters;
+
+    private String name;
+    private String desc;
 
     @Before
     public void setUp() throws Exception {
@@ -25,9 +29,6 @@ public class InterrogatorTest {
 
         when(moduleIntrospector.getActions()).thenReturn(moduleActions);
         when(moduleIntrospector.getParameters()).thenReturn(moduleParameters);
-
-
-        interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{});
     }
 
     @After
@@ -36,9 +37,24 @@ public class InterrogatorTest {
     }
 
     @Test
-    public void testTorture() throws Exception {
-        //TODO: implement
-        Assert.fail("Not implemented");
+    public void testTortureSetOneName() throws Exception {
+        Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--name", JOHN_FROSTER});
         interrogator.torture();
+
+        org.junit.Assert.assertThat(name, CoreMatchers.is(JOHN_FROSTER));
+    }
+
+    @Parameter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Parameter
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
     }
 }
