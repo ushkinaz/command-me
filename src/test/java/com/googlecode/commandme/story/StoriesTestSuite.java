9fc07865a9851a9e5590083856fa17a61327ff1e
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-20 21:42:39 +0400

    Experimental suites.

---
+++ src/test/java/com/googlecode/commandme/story/StoriesTestSuite.java
@@ -0,0 +1,15 @@
+package com.googlecode.commandme.story;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Dmitry Sidorenko
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Story_13446725.class,
+        Story_13446727.class,
+        Story_13644361.class})
+public class StoriesTestSuite {
+}
