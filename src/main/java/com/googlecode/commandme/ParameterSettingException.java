ed930b5c4d5f53b9e0045870f0a5a16d7635169d
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-21 06:22:01 +0400

    [continues #13446729] One step closer to finish
    --HG--
    rename : src/main/java/com/googlecode/commandme/ActionInvocationException.java => src/main/java/com/googlecode/commandme/ParameterSettingException.java

---
+++ src/main/java/com/googlecode/commandme/ParameterSettingException.java
@@ -0,0 +1,15 @@
+package com.googlecode.commandme;
+
+/**
+ * @author Dmitry Sidorenko
+ */
+public class ParameterSettingException extends CliException {
+
+  public ParameterSettingException(String message) {
+    super(message);
+  }
+
+  public ParameterSettingException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
