1a7c9785d41bd127fd02f1d662808052f46d3b2c
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-21 06:22:01 +0400

    [continues #13446729] One step closer to finish
    --HG--
    rename : src/main/java/com/googlecode/commandme/ActionInvocationException.java => src/main/java/com/googlecode/commandme/ParameterSettingException.java

--- src/main/java/com/googlecode/commandme/impl/interrogator/Interrogator.java
+++ src/main/java/com/googlecode/commandme/impl/interrogator/Interrogator.java
@@ -18,12 +18,14 @@ package com.googlecode.commandme.impl.interrogator;
 
 import com.googlecode.commandme.ActionInvocationException;
 import com.googlecode.commandme.CliException;
+import com.googlecode.commandme.ParameterSettingException;
 import com.googlecode.commandme.impl.introspector.ActionDefinition;
 import com.googlecode.commandme.impl.introspector.ModuleIntrospector;
 import com.googlecode.commandme.impl.introspector.ParameterDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 /**
@@ -32,98 +34,126 @@ import java.lang.reflect.InvocationTargetException;
  * @author Dmitry Sidorenko
  */
 public class Interrogator<T> {
-    @SuppressWarnings({"UnusedDeclaration"})
-    private static final Logger LOGGER = LoggerFactory.getLogger(Interrogator.class);
+  @SuppressWarnings({"UnusedDeclaration"})
+  private static final Logger LOGGER = LoggerFactory.getLogger(Interrogator.class);
 
-    private final T                  module;
-    private final ModuleIntrospector moduleIntrospector;
-    private final String[]           arguments;
+  private final T module;
+  private final ModuleIntrospector moduleIntrospector;
+  private final String[] arguments;
 
+  /**
+   * A constructor.
+   *
+   * @param module             an instance to interrogate
+   * @param moduleIntrospector initialized introspector
+   * @param arguments          actual arguments
+   */
+  Interrogator(T module, ModuleIntrospector moduleIntrospector, String[] arguments) {
+    this.module = module;
+    this.moduleIntrospector = moduleIntrospector;
+    this.arguments = arguments;
+  }
+
+  /**
+   * Does actual injecting and calls actions
+   */
+  public void torture() {
+    TokenType currentToken = TokenType.ACTION;
+    ParameterDefinition parameterDef = null;
+    for (String argument : arguments) {
+      LOGGER.debug("Parsing: {}" + argument);
+      if (argument.startsWith("--")) {
+        LOGGER.debug("Found parameter");
+        currentToken = TokenType.PARAMETER;
+        parameterDef = findParameterDefinition(argument.substring(2));
+      } else {
+        switch (currentToken) {
+          case PARAMETER:
+            LOGGER.debug("Found value");
+            currentToken = TokenType.VALUE;
+            setParameterValue(parameterDef, argument);
+            break;
+          case VALUE:
+          case ACTION:
+            LOGGER.debug("Found action");
+            currentToken = TokenType.ACTION;
+            callAction(argument);
+            break;
+        }
+      }
+    }
+  }
+
+  private void setParameterValue(ParameterDefinition parameterDefinition, String value) {
+    Class parameterType = parameterDefinition.getType();
+    if (parameterType.isPrimitive()) {
+      parameterType = findWrapperClass(parameterType);
+    }
+    try {
+      LOGGER.debug("Setting value '{}' to {}", new Object[]{value, parameterDefinition});
+      Constructor constructor = parameterType.getConstructor(String.class);
+      Object convertedValue = constructor.newInstance(value);
+      parameterDefinition.getWriterMethod().invoke(module, convertedValue);
+    } catch (NoSuchMethodException e) {
+      LOGGER.warn("Can't find appropriate constructor for " + parameterType, e);
+      throw new ParameterSettingException("Can't find appropriate constructor for " + parameterType, e);
+    } catch (InstantiationException e) {
+      LOGGER.warn("Can't convert value from String '" + value + "' to " + parameterType, e);
+      throw new ParameterSettingException("Can't convert value from String '" + value + "' to " + parameterType, e);
+    } catch (IllegalAccessException e) {
+      throw new ParameterSettingException("Can't access method", e);
+    } catch (InvocationTargetException e) {
+      throw new ParameterSettingException("Constructor throws an exception", e);
+    }
+  }
+
+  private Class findWrapperClass(Class primitiveClass) {
+    assert primitiveClass.isPrimitive();
+
+    try {
+      if (primitiveClass.equals(Integer.TYPE)) {
+        return Integer.class;
+      }
+      //Constructing wrapper class name from primitive class name
+      return Class.forName("java.lang." + Character.toUpperCase(primitiveClass.getName().charAt(0)) + primitiveClass.getName().substring(1));
+    } catch (ClassNotFoundException e) {
+      LOGGER.warn("Can't find class for primitive type " + primitiveClass, e);
+      throw new ParameterSettingException("Can't find class for primitive type " + primitiveClass, e);
+    }
+  }
+
+  private ParameterDefinition findParameterDefinition(String name) {
+    return moduleIntrospector.getParameters().getByLongName(name);
+  }
+
+  private void callAction(String longActionName) {
+    ActionDefinition definition = moduleIntrospector.getActions().getByLongName(longActionName);
+    if (definition != null) {
+      LOGGER.debug("Executing action: {}", definition);
+      try {
+        definition.getMethod().invoke(module);
+      } catch (Exception e) {
+        LOGGER.warn("Exception", e);
+        throw new CliException("Exception invoking action " + definition, e);
+      }
+    } else {
+      LOGGER.warn("Can't find action:" + longActionName);
+      throw new ActionInvocationException("Can't find action:" + longActionName);
+    }
+  }
+
+  private enum TokenType {
     /**
-     * A constructor.
-     *
-     * @param module             an instance to interrogate
-     * @param moduleIntrospector initialized introspector
-     * @param arguments          actual arguments
+     * Parameter with value
      */
-    Interrogator(T module, ModuleIntrospector moduleIntrospector, String[] arguments) {
-        this.module = module;
-        this.moduleIntrospector = moduleIntrospector;
-        this.arguments = arguments;
-    }
-
+    PARAMETER,
     /**
-     * Does actual injecting and calls actions
+     * Action
      */
-    public void torture() {
-        TokenType currentToken = TokenType.ACTION;
-        ParameterDefinition parameterDef = null;
-        for (String argument : arguments) {
-            LOGGER.debug("Parsing: " + argument);
-            if (argument.startsWith("--")) {
-                LOGGER.debug("Found parameter");
-                currentToken = TokenType.PARAMETER;
-                parameterDef = findParameterDefinition(argument.substring(2));
-            } else {
-                switch (currentToken) {
-                    case PARAMETER:
-                        LOGGER.debug("Found value");
-                        currentToken = TokenType.VALUE;
-                        setParameterValue(parameterDef, argument);
-                        break;
-                    case VALUE:
-                    case ACTION:
-                        LOGGER.debug("Found action");
-                        currentToken = TokenType.ACTION;
-                        callAction(argument);
-                        break;
-                }
-            }
-        }
-    }
-
-    private void setParameterValue(ParameterDefinition parameterDefinition, String value) {
-        try {
-            parameterDefinition.getWriterMethod().invoke(module, value);
-        } catch (IllegalAccessException e) {
-            throw new CliException(e);
-        } catch (InvocationTargetException e) {
-            throw new CliException(e);
-        }
-    }
-
-    private ParameterDefinition findParameterDefinition(String name) {
-        return moduleIntrospector.getParameters().getByLongName(name);
-    }
-
-    private void callAction(String longActionName) {
-        ActionDefinition definition = moduleIntrospector.getActions().getByLongName(longActionName);
-        if (definition != null) {
-            LOGGER.debug("Executing action:" + definition);
-            try {
-                definition.getMethod().invoke(module);
-            } catch (Exception e) {
-                LOGGER.warn("Exception", e);
-                throw new CliException("Exception invoking action " + definition, e);
-            }
-        } else {
-            LOGGER.warn("Can't find action:" + longActionName);
-            throw new ActionInvocationException("Can't find action:" + longActionName);
-        }
-    }
-
-    private enum TokenType {
-        /**
-         * Parameter with value
-         */
-        PARAMETER,
-        /**
-         * Action
-         */
-        ACTION,
-        /**
-         * Value of a parameter
-         */
-        VALUE
-    }
+    ACTION,
+    /**
+     * Value of a parameter
+     */
+    VALUE
+  }
 }
--- src/test/java/com/googlecode/commandme/impl/interrogator/InterrogatorTest.java
+++ src/test/java/com/googlecode/commandme/impl/interrogator/InterrogatorTest.java
@@ -1,5 +1,6 @@
 package com.googlecode.commandme.impl.interrogator;
 
+import com.googlecode.commandme.ParameterSettingException;
 import com.googlecode.commandme.annotations.Parameter;
 import com.googlecode.commandme.impl.introspector.*;
 import org.hamcrest.CoreMatchers;
@@ -12,55 +13,113 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class InterrogatorTest {
-    private static final String JOHN_FROSTER = "John Froster";
-    private static final String DEVELOPER    = "developer";
-    private ModuleIntrospector<InterrogatorTest> moduleIntrospector;
+  private static final String JOHN_FROSTER = "John Froster";
+  private static final String DEVELOPER = "developer";
+  private ModuleIntrospector<InterrogatorTest> moduleIntrospector;
 
-    private String name;
-    private String desc;
+  private String name;
+  private String desc;
+  private int id;
+  private int amount;
 
-    @Before
-    public void setUp() throws Exception {
-        moduleIntrospector = mock(ModuleIntrospector.class);
-        ModuleActions moduleActions = new ActionsIntrospector<InterrogatorTest>(InterrogatorTest.class);
-        moduleActions.inspect();
+  @Before
+  public void setUp() throws Exception {
+    //noinspection unchecked
+    moduleIntrospector = mock(ModuleIntrospector.class);
+    ModuleActions moduleActions = new ActionsIntrospector<InterrogatorTest>(InterrogatorTest.class);
+    moduleActions.inspect();
 
-        ModuleParameters moduleParameters = new ParametersIntrospector<InterrogatorTest>(InterrogatorTest.class);
-        moduleParameters.inspect();
+    ModuleParameters moduleParameters = new ParametersIntrospector<InterrogatorTest>(InterrogatorTest.class);
+    moduleParameters.inspect();
 
-        when(moduleIntrospector.getActions()).thenReturn(moduleActions);
-        when(moduleIntrospector.getParameters()).thenReturn(moduleParameters);
-    }
+    when(moduleIntrospector.getActions()).thenReturn(moduleActions);
+    when(moduleIntrospector.getParameters()).thenReturn(moduleParameters);
+  }
 
-    @After
-    public void tearDown() throws Exception {
+  @After
+  public void tearDown() throws Exception {
 
-    }
+  }
 
-    @Test
-    public void testTortureSetOneName() throws Exception {
-        Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--name", JOHN_FROSTER});
-        interrogator.torture();
+  @Test
+  public void testTortureSetOneName() throws Exception {
+    Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--name", JOHN_FROSTER});
+    interrogator.torture();
 
-        assertThat(name, CoreMatchers.is(JOHN_FROSTER));
-    }
+    assertThat(name, CoreMatchers.is(JOHN_FROSTER));
+  }
 
-    @Test
-    public void testTortureSetTwoNames() throws Exception {
-        Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--name", JOHN_FROSTER, "--desc", DEVELOPER});
-        interrogator.torture();
+  @Test
+  public void testTortureSetTwoNames() throws Exception {
+    Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--name", JOHN_FROSTER, "--desc", DEVELOPER});
+    interrogator.torture();
 
-        assertThat(name, CoreMatchers.is(JOHN_FROSTER));
-        assertThat(desc, CoreMatchers.is(DEVELOPER));
-    }
+    assertThat(name, CoreMatchers.is(JOHN_FROSTER));
+    assertThat(desc, CoreMatchers.is(DEVELOPER));
+  }
 
-    @Parameter
-    public void setName(String name) {
-        this.name = name;
-    }
+  @Test
+  public void testSetInteger() throws Exception {
+    Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--id", "555"});
+    interrogator.torture();
 
-    @Parameter
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
+    assertThat(id, CoreMatchers.is(555));
+  }
+
+  @Test(expected = ParameterSettingException.class)
+  public void testSetIntegerWrongFormat() throws Exception {
+    Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--id", "555.0"});
+    interrogator.torture();
+
+    assertThat(id, CoreMatchers.is(555));
+  }
+
+  @Test(expected = ParameterSettingException.class)
+  public void testSetByteOutOf() throws Exception {
+    Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--idByte", "555"});
+    interrogator.torture();
+
+    assertThat(id, CoreMatchers.is(555));
+  }
+
+  @Test
+  public void testSetByte() throws Exception {
+    Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--idByte", "122"});
+    interrogator.torture();
+
+    assertThat(id, CoreMatchers.is(122));
+  }
+
+  @Test
+  public void testSetInt() throws Exception {
+    Interrogator<InterrogatorTest> interrogator = new Interrogator<InterrogatorTest>(this, moduleIntrospector, new String[]{"--amount", "122"});
+    interrogator.torture();
+
+    assertThat(amount, CoreMatchers.is(122));
+  }
+
+  @Parameter
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Parameter
+  public void setDesc(String desc) {
+    this.desc = desc;
+  }
+
+  @Parameter
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  @Parameter
+  public void setIdByte(byte id) {
+    this.id = id;
+  }
+
+  @Parameter
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
 }
