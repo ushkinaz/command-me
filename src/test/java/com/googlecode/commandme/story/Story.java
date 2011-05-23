b51e046a2dacafbafb6a1b343c4488ea82cd0bae
Dmitry Sidorenko <rustydragon@gmail.com>
2011-05-23 16:07:38 +0400

    Really small refactoring

--- src/test/java/com/googlecode/commandme/story/Story.java
+++ src/test/java/com/googlecode/commandme/story/Story.java
@@ -10,18 +10,18 @@ import org.slf4j.LoggerFactory;
 /**
  * @author Dmitry Sidorenko
  */
-public class Story<T extends Story> {
+public abstract class Story<T extends Story> {
     @SuppressWarnings({"unused"})
     private static final Logger LOGGER = LoggerFactory.getLogger(Story.class);
     protected CommandLine<T> commandLine;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUpStory() throws Exception {
         commandLine = (CommandLine<T>) CLIParser.createModule(getClass());
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDownStory() throws Exception {
         commandLine.shutdown();
     }
 }
